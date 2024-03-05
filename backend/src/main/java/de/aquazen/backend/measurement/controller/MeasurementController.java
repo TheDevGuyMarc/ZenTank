fcffde4b345e291d436f0dc1e52461d6ab42e678
FrostTrauma <thedevguymarc@gmail.com>
2024-03-05 09:17:29 +0100

    finishing controller scaffoldings

--- backend/src/main/java/de/aquazen/backend/measurement/controller/MeasurementController.java
+++ backend/src/main/java/de/aquazen/backend/measurement/controller/MeasurementController.java
@@ -1,9 +1,54 @@
 package de.aquazen.backend.measurement.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import de.aquazen.backend.measurement.domain.Measurement;
+import de.aquazen.backend.measurement.service.MeasurementService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/measurements")
 public class MeasurementController {
+    private final MeasurementService measurementService;
+
+    public MeasurementController(MeasurementService measurementService) {
+        this.measurementService = measurementService;
+    }
+
+    @GetMapping
+    public List<MeasurementDTO> list() {
+        return this.measurementService.findAllMeasurements()
+                .stream()
+                .map(MeasurementDTO::new)
+                .toList();
+    }
+
+    @GetMapping("/{id}")
+    public MeasurementDTO read(@PathVariable @Min(1) Long id) {
+        return this.measurementService.findMeasurementById(id)
+                .map(MeasurementDTO::new)
+                .orElseThrow();
+    }
+
+    @PostMapping
+    public MeasurementDTO create(@RequestBody @Valid @NotNull @NotEmpty Measurement measurement) {
+        return new MeasurementDTO(this.measurementService.createMeasurement(measurement));
+    }
+
+    @PutMapping("/{id}")
+    public MeasurementDTO update(@PathVariable @Min(1) Long id, @RequestBody @Valid @NotNull @NotEmpty Measurement measurement) {
+        return new MeasurementDTO(this.measurementService.updateMeasurement(id, measurement));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable @Min(1) Long id) {
+        this.measurementService.deleteMeasurement(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
--- backend/src/main/java/de/aquazen/backend/timer/controller/TimerController.java
+++ backend/src/main/java/de/aquazen/backend/timer/controller/TimerController.java
@@ -1,4 +1,54 @@
 package de.aquazen.backend.timer.controller;
 
+import de.aquazen.backend.timer.domain.Timer;
+import de.aquazen.backend.timer.service.TimerService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/timers")
 public class TimerController {
+    private final TimerService timerService;
+
+    public TimerController(TimerService timerService) {
+        this.timerService = timerService;
+    }
+
+    @GetMapping
+    public List<TimerDTO> list() {
+        return this.timerService.findAllTimers()
+                .stream()
+                .map(TimerDTO::new)
+                .toList();
+    }
+
+    @GetMapping("/{id}")
+    public TimerDTO read(@PathVariable @Min(1) Long id) {
+        return this.timerService.findTimerById(id)
+                .map(TimerDTO::new)
+                .orElseThrow();
+    }
+
+    @PostMapping
+    public TimerDTO create(@RequestBody @Valid @NotNull @NotEmpty Timer timer) {
+        return new TimerDTO(this.timerService.createTimer(timer));
+    }
+
+    @PutMapping("/{id}")
+    public TimerDTO update(@PathVariable @Min(1) Long id, @RequestBody @Valid @NotNull @NotEmpty Timer timer) {
+        return new TimerDTO(this.timerService.updateTimer(id, timer));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable @Min(1) Long id) {
+        this.timerService.deleteTimer(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
