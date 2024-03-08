2e251b641d5c2d55b0a84d0df1482415c080c1f3
FrostTrauma <thedevguymarc@gmail.com>
2024-03-08 14:56:26 +0100

    Implementing Dosing Systems

--- backend/src/main/java/de/aquazen/backend/dosing/domain/Dosing.java
+++ backend/src/main/java/de/aquazen/backend/dosing/domain/Dosing.java
@@ -1,11 +1,30 @@
 package de.aquazen.backend.dosing.domain;
 
+import de.aquazen.backend.dosing.repository.DosingEntity;
+import de.aquazen.backend.drivers.domain.Pin;
+import de.aquazen.backend.jacks.domain.Jack;
+import de.aquazen.backend.regiment.domain.Regiment;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class Dosing {
+    private Long id;
+    private String name;
+    private Pin pin;
+    private Jack jack;
+    private List<Regiment> regiments;
+
+    public Dosing(DosingEntity entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.pin = entity.getPin();
+        this.jack = new Jack(entity.getJack());
+        this.regiments = entity.getRegiments().stream().map(Regiment::new).toList();
+    }
 }
--- backend/src/main/java/de/aquazen/backend/dosing/service/DosingService.java
+++ backend/src/main/java/de/aquazen/backend/dosing/service/DosingService.java
@@ -1,6 +1,8 @@
 package de.aquazen.backend.dosing.service;
 
 import de.aquazen.backend.dosing.domain.Dosing;
+import de.aquazen.backend.dosing.exception.DosingNotFoundException;
+import de.aquazen.backend.dosing.repository.DosingEntity;
 import de.aquazen.backend.dosing.repository.DosingRepository;
 import org.springframework.stereotype.Service;
 
@@ -16,22 +18,42 @@ public class DosingService {
     }
 
     public List<Dosing> findAllDosingSystems() {
-
+        return this.dosingRepository.findAll()
+            .stream()
+            .map(Dosing::new)
+            .toList();
     }
 
     public Optional<Dosing> findDosingSystemById(Long id) {
-
+        return this.dosingRepository.findById(id)
+            .map(Dosing::new);
     }
 
     public Dosing createDosingSystem(Dosing dosing) {
-
+        return new Dosing(this.dosingRepository.saveAndFlush(new DosingEntity(dosing)));
     }
 
     public Dosing updateDosingSystem(Long id, Dosing dosing) {
+        Optional<Dosing> existingElement = this.dosingRepository.findById(id)
+            .map(Dosing::new);
 
+        if (existingElement.isPresent()) {
+            Dosing updatedElement = existingElement.get();
+
+            updatedElement.setId(dosing.getId());
+            updatedElement.setName(dosing.getName());
+            updatedElement.setPin(dosing.getPin());
+            updatedElement.setJack(dosing.getJack());
+            updatedElement.setRegiments(dosing.getRegiments());
+
+            return new Dosing(this.dosingRepository.saveAndFlush(new DosingEntity(updatedElement)));
+        }
+        else {
+            throw new DosingNotFoundException("Couldn't find the dosing system entry you requested with id: " + id);
+        }
     }
 
     public void deleteDosingSystem(Long id) {
-
+        this.dosingRepository.deleteById(id);
     }
 }
