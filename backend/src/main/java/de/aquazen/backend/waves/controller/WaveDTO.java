c7c794ca49d845969c1d645e18e454418796d4eb
FrostTrauma <thedevguymarc@gmail.com>
2024-03-08 13:52:29 +0100

    Implementing Wave Systems

--- backend/src/main/java/de/aquazen/backend/waves/controller/WaveDTO.java
+++ backend/src/main/java/de/aquazen/backend/waves/controller/WaveDTO.java
@@ -1,7 +1,25 @@
 package de.aquazen.backend.waves.controller;
 
+import de.aquazen.backend.channel.controller.ChannelDTO;
+import de.aquazen.backend.jacks.controller.JackDTO;
+import de.aquazen.backend.waves.domain.Wave;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 public class WaveDTO {
+    private final Long id;
+    private final String name;
+    private final boolean state;
+    private final JackDTO jack;
+    private final List<ChannelDTO> channels;
+
+    public WaveDTO(Wave entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.state = entity.isState();
+        this.jack = new JackDTO(entity.getJack());
+        this.channels = entity.getChannels().stream().map(ChannelDTO::new).toList();
+    }
 }
